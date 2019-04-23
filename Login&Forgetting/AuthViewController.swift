9c08014a7c8d48ef7c6053a5bed9c1ff0039de3f
r0ger27 <rsafin90@gmail.com>
2019-04-24 00:30:10 +0300

    Initial Commit

---
+++ Login&Forgetting/AuthViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AuthViewController.swift
+//  Login&Forgetting
+//
+//  Created by Руслан Сафин on 23/04/2019.
+//  Copyright © 2019 Ruslan Safin. All rights reserved.
+//
+
+import UIKit
+
+class AuthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Login&Forgetting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Login&Forgetting
+//
+//  Created by Руслан Сафин on 23/04/2019.
+//  Copyright © 2019 Ruslan Safin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
