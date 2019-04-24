2ebb53154a15a60d582ed56548b80a7b19cf8354
r0ger27 <rsafin90@gmail.com>
2019-04-24 19:25:32 +0300

    Final Version 1.0

--- Login&Forgetting/AuthViewController.swift
+++ Login&Forgetting/AuthViewController.swift
@@ -2,12 +2,25 @@
 //  AuthViewController.swift
 //  Login&Forgetting
 //
-//  Created by Руслан Сафин on 23/04/2019.
+//  Created by Ruslan Safin on 23/04/2019.
 //  Copyright © 2019 Ruslan Safin. All rights reserved.
 //
 
 import UIKit
 
 class AuthViewController: UIViewController {
-
+    
+    // MARK: - Vars
+    var textAuth = ""
+    
+    // MARK: - Outlets
+    @IBOutlet var authText: UILabel!
+    
+    // MARK: - Override methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        authText.text = textAuth
+        authText.numberOfLines = 0
+    }
 }
--- Login&Forgetting/ViewController.swift
+++ Login&Forgetting/ViewController.swift
@@ -2,15 +2,19 @@
 //  ViewController.swift
 //  Login&Forgetting
 //
-//  Created by Руслан Сафин on 23/04/2019.
+//  Created by Ruslan Safin on 23/04/2019.
 //  Copyright © 2019 Ruslan Safin. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     
+    // MARK: - Vars
+    let authUsername = "Ruslan"
+    let authPassword = "123456"
     
+    // MARK: - Outlets
     @IBOutlet var usernameField: UITextField!
     @IBOutlet var passwordField: UITextField!
     
@@ -18,40 +22,60 @@ class ViewController: UIViewController {
     @IBOutlet var forgotLogin: UIButton!
     @IBOutlet var forgotPassword: UIButton!
     
-    
-    let authUsername = "Ruslan"
-    let authPassword = "123456"
-    
-    enum UserAction {
-        case onlyLogin, onlyPassword, wrongLogin, wrongPassword, forgotLogin, forgotPass, allGood
+    // MARK: - Override methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.usernameField.delegate = self
+        self.passwordField.delegate = self
     }
     
-    
-    
-/*
-    
-    func userActions(username: String, password: String, sender: UIButton) {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destination = segue.destination as! AuthViewController
         
-        switch UserAction.self {
-        case .onlyLogin: print(1)
-            
-        case .onlyPassword: print(1)
-            
-        case .wrongLogin: print(1)
-            
-        case .wrongPassword: print(1)
-            
-        case .forgotLogin: print(1)
-            
-        case .forgotPass: print(1)
-            
-        case .allGood:
-            if username == authUsername && password == authPassword {
-                print(1)
+        if segue.identifier == "loginButton" {
+            if usernameField.text == authUsername && passwordField.text == authPassword {
+                destination.navigationItem.title = "Welcome!"
+                destination.textAuth = "Hello \(authUsername)! \n" + "Your password is \(authPassword)"
+            } else if usernameField.text!.isEmpty == true || passwordField.text!.isEmpty == true {
+                showAlert(message: "Please, enter data")
+            } else if usernameField.text != authUsername || passwordField.text != authPassword {
+                showAlert(message: "Please, check the entered data")
             }
         }
-    } */
-
+        
+        if segue.identifier == "forgotLogin" {
+            destination.navigationItem.title = "Forgot login?"
+            destination.textAuth = "Remember, your login is '\(authUsername)'"
+        }
+        
+        if segue.identifier == "forgotPass" {
+            destination.navigationItem.title = "Forgot password?"
+            destination.textAuth = "Remember, your password is '\(authPassword)'"
+        }
+    }
     
+    // MARK: - Private Methods
+    private func showAlert(message: String) {
+        let alert = UIAlertController(title: "Error!", message: message, preferredStyle: .alert)
+        let okayAction = UIAlertAction(title: "OK", style: .destructive)
+        
+        alert.addAction(okayAction)
+        
+        present(alert, animated: true)
+    }
 }
 
+// MARK: Work with the keyboard
+extension ViewController {
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+}
